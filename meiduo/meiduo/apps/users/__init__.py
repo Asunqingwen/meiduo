34a7478cdcf8e66666afb95c37ceb016e8c9d638
Asunqingwen <sqw123az@sina.com>
2019-11-28 14:04:39 +0800

    Initial commit

---
+++ meiduo/meiduo/apps/users/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/28 0028 14:03
+# @Author  : 没有蜡笔的小新
+# @E-mail  : sqw123az@sina.com
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/Asunqingwen
+# @GitHub  ：https://github.com/Asunqingwen
+# @WebSite : labixiaoxin.me
